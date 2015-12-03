49405db451baeb03c7a32dfaf9424f76868e962f
alfianhidayat <alfian08.ah@gmail.com>
2015-12-04 05:21:27 +0700

    pemilu

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -59,7 +59,7 @@
     </a>
     <div class="row"><a href="">
       <div class="col l6 s12">
-        <h5 class="white-text">PPHU</h5>
+        <h5 class="white-text">PPUH</h5>
         <p class="grey-text text-lighten-4"></p>
       </div>
     </a>
